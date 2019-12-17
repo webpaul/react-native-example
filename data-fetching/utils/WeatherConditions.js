82ab00738c8880f2d96dd6e6491f29ad5d66071e
Paul B <a@b.com>
2019-12-17 11:30:02 -0600

    Data fetching example

---
+++ data-fetching/utils/WeatherConditions.js
@@ -0,0 +1,51 @@
+export const weatherConditions = {
+    Rain: {
+      color: '#005BEA',
+      title: 'Raining',
+      subtitle: 'Settle in, get coffee',
+      icon: 'weather-rainy'
+    },
+    Clear: {
+      color: '#f7b733',
+      title: 'So Sunny',
+      subtitle: 'It burns',
+      icon: 'weather-sunny'
+    },
+    Thunderstorm: {
+      color: '#616161',
+      title: 'A Storm is a coming',
+      subtitle: 'Because the Gods are angry',
+      icon: 'weather-lightning'
+    },
+    Clouds: {
+      color: '#1F1C2C',
+      title: 'Clouds',
+      subtitle: 'Everywhere',
+      icon: 'weather-cloudy'
+    },
+  
+    Snow: {
+      color: '#00d2ff',
+      title: 'Snow',
+      subtitle: 'Get out and build a snow fort',
+      icon: 'weather-snowy'
+    },
+    Drizzle: {
+      color: '#076585',
+      title: 'Drizzle',
+      subtitle: 'Partially raining...',
+      icon: 'weather-hail'
+    },
+    Haze: {
+      color: '#66A6FF',
+      title: 'Haze',
+      subtitle: 'Purple Haze, or perhaps gray',
+      icon: 'weather-hail'
+    },
+    Mist: {
+      color: '#3CD3AD',
+      title: 'Mist',
+      subtitle: "Don't call me Misty",
+      icon: 'weather-fog'
+    }
+  };
