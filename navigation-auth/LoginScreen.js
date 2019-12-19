6bc4fa8e3593645b3e5b6ade1675cbd421b85fcc
Paul B <a@b.com>
2019-12-19 16:53:03 -0600

    Added login request

--- navigation-auth/LoginScreen.js
+++ navigation-auth/LoginScreen.js
@@ -1,32 +1,69 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 
 export default class LoginScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.restServer = 'http://172.20.50.240';
+
+    this.state = {
+      user: '',
+      pass: ''
+    };
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text>Login screen</Text>
+        <TextInput autoCapitalize="none"
+                   autoCorrect={false} 
+                   autoFocus
+                   keyboardType="default" 
+                   maxLength={ 20 }
+                   placeholder="Username"
+                   returnKeyType="done"
+                   enablesReturnKeyAutomatically
+                   style={styles.username}
+                   onChangeText={(user) => this.setState({user})}
+                   />
+        <TextInput autoCapitalize="none"
+                   autoCorrect={false} 
+                   autoFocus
+                   keyboardType="default" 
+                   maxLength={ 20 }
+                   placeholder="Pass"
+                   returnKeyType="done"
+                   enablesReturnKeyAutomatically
+                   style={styles.username}
+                   onChangeText={(pass) => this.setState({pass})}
+                   />
+
         <Button
           title="Let me innnnn"
-          onPress={() =>
-            this.props.navigation.navigate('App')
-          }
+          onPress={() => {
+            console.log(this.state);
+
+            const payload = { ...this.state };
+
+            fetch(`${this.restServer}/login`, {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify(payload)
+            }).then(x => {
+              console.log(payload);
+              console.log(x);
+              this.props.navigation.navigate('App')
+            }).catch(error => console.log(error));
+
+          }}
         />
       </View>
     );
   }
 }
-
-// const payload = {
-//   message: text
-// };
-// fetch(`${pusherConfig.restServer}/users/${this.props.name}/messages`, {
-// method: 'POST',
-// headers: {
-//   'Content-Type': 'application/json'
-// },
-// body: JSON.stringify(payload)
-// });
 
 const styles = StyleSheet.create({
   container: {
